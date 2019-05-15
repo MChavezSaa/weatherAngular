9f7107482bb7080a22e22c9147616e1503076460
Maty <matroxmega@gmail.com>
2019-05-15 17:58:24 -0400

    add getWeather on weather services

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  apiKey: string= '7c6b1454c32b1204231cb8e9c45d23a7';
+  URI: string='';
+
+  constructor(private http: HttpClient) { 
+    this.URI=`api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
+  }
+
+  getWeather(city: string, country: string){
+    return this.http.get(`${this.URI}${city},${country}`);
+  }
+
+  
 }
